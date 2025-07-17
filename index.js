f5f4458cfcd9d7ca2959c8f406a3ff09f00dc4c9
Zbigniew Szepczynski <zbigniew.szepczynski@ibm.com>
2025-07-17 11:33:11 +0200

    fix

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 require("dotenv").config({ path: "./config.env" });
 
-const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 
-const sourceAccount = 'apptiospmdemo.tpondemand.com';
-const sourceRegion = 'eu-west-1';
-const sourceBucket = 'apptio-tp-snapshots';
-const sourceSnapshotId = '6491e336-5ea7-447a-98e5-99dc6de8e617';
-const sourceSnapshotName = 'arrow_fr_assignments';
+const sourceAccount = "apptiospmdemo.tpondemand.com";
+const sourceRegion = "eu-west-1";
+const sourceBucket = "apptio-tp-snapshots";
+const sourceSnapshotId = "6491e336-5ea7-447a-98e5-99dc6de8e617";
+const sourceSnapshotName = "arrow_fr_assignments";
 const sourceAccessKeyId = process.env.SOURCE_ACCESS_KEY_ID;
 const sourceAccessKey = process.env.SOURCE_ACCESS_KEY;
 const sourceSessionToken = process.env.SOURCE_ACCESS_SESSION_TOKEN;
 
+// TOXIC EXAMPLE
 const targetAccount = 'toxic2.tpondemand.com';
 const targetRegion = 'us-west-2';
 const targetBucket = 'apptio-tp-snapshots-stg';
@@ -20,12 +25,27 @@ const targetAccessKeyId = process.env.TARGET_ACCESS_KEY_ID;
 const targetAccessKey = process.env.TARGET_ACCESS_KEY;
 const targetSessionToken = process.env.TARGET_ACCESS_SESSION_TOKEN;
 
+// LOCAL EXAMPLE 
+// let targetAccount = "tplocal.com";
+// const targetRegion = "us-east-1";
+// const targetBucket = "apptio-tp-snapshots-stg";
+// const targetSnapshotId = "23b14ee0-ac26-450f-8144-1ff62e01b4b9";
+// const targetSnapshotName = "test";
+// const targetAccessKeyId = "test";
+// const targetAccessKey = "test";
+// const targetSessionToken = "apptio-tp-snapshots-stg";
+
+const targetAwsEndpoint = process.env.TARGET_AWS_ENDPOINT;
+const targets3ForcePathStyle = Boolean(targetAwsEndpoint);
+
+console.log("AWS Endpoint", targetAwsEndpoint);
+
 const sourceS3 = new S3Client({
   region: sourceRegion,
   credentials: {
     accessKeyId: sourceAccessKeyId,
     secretAccessKey: sourceAccessKey,
-    sessionToken: sourceSessionToken
+    sessionToken: sourceSessionToken,
   },
 });
 
@@ -34,79 +54,123 @@ const targetS3 = new S3Client({
   credentials: {
     accessKeyId: targetAccessKeyId,
     secretAccessKey: targetAccessKey,
-    sessionToken: targetSessionToken
+    sessionToken: targetSessionToken,
   },
+  endpoint: targetAwsEndpoint,
+  s3ForcePathStyle: targets3ForcePathStyle,
 });
 
-const getMetaPartLocation = (account, snapshotName, snapshotId, part) =>{
-      return `${account}/${snapshotName}_${snapshotId}/${part}`;
-}
+const getMetaPartLocation = (account, snapshotName, snapshotId, part) => {
+  return `${account}/${snapshotName}_${snapshotId}/${part}`;
+};
 
-const getTpDbBackupLocation = async (s3, bucket, account, snapshotName, snapshotId)=>{
-    const fileLocation = getMetaPartLocation(account,snapshotName,snapshotId, 'tp_db.bak');
-    console.log("Reading meta file tp_db.bak from: ", fileLocation);
-    const res = await s3.send(new GetObjectCommand({Bucket: bucket, Key: fileLocation}));
-    const metaContent = JSON.parse(await res.Body.transformToString());
-    return metaContent.Key;
-}
+const getTpDbBackupLocation = async (s3, bucket, metaFileLocation) => {
+  console.log("Reading meta file tp_db.bak from:", metaFileLocation, bucket);
+  const res = await s3.send(
+    new GetObjectCommand({ Bucket: bucket, Key: metaFileLocation })
+  );
+  const metaContent = JSON.parse(await res.Body.transformToString());
+  return metaContent.Key;
+};
 
+const moveTpDb = async () => {
+  const sourceMetaFileLocation = getMetaPartLocation(
+    sourceAccount,
+    sourceSnapshotName,
+    sourceSnapshotId,
+    "tp_db.bak"
+  );
 
-const moveTpDb = async ()=>{
-   const sourceTpDbLocation = await getTpDbBackupLocation(
-     sourceS3,
-     sourceBucket,
-     sourceAccount,
-     sourceSnapshotName,
-     sourceSnapshotId
-   );
+  const sourceTpDbLocation = await getTpDbBackupLocation(
+    sourceS3,
+    sourceBucket,
+    sourceMetaFileLocation
+  );
 
-  const targetTpDbLocation = await getTpDbBackupLocation(targetS3, targetBucket, targetAccount, targetSnapshotName, targetSnapshotId);
+  const targetMetaFileLocation = getMetaPartLocation(
+    targets3ForcePathStyle ? `${targetBucket}/${targetAccount}` : targetAccount,
+    targetSnapshotName,
+    targetSnapshotId,
+    "tp_db.bak"
+  );
 
-   console.log(`Uploading db file from ${sourceTpDbLocation}, ${sourceBucket} to ${targetTpDbLocation}, ${targetBucket}`);
+  let targetTpDbLocation = await getTpDbBackupLocation(
+    targetS3,
+    targetBucket,
+    targetMetaFileLocation
+  );
+  if (targets3ForcePathStyle) {
+    targetTpDbLocation = `${targetBucket}/${targetTpDbLocation}`;
+  }
 
-  const databaseFile = await sourceS3.send(new GetObjectCommand({Bucket: sourceBucket, Key: sourceTpDbLocation}));
+  console.log(
+    `Uploading db file from ${sourceTpDbLocation}, ${sourceBucket} to ${targetTpDbLocation}, ${targetBucket}`
+  );
 
-  const data = await databaseFile.Body.transformToByteArray() // readable stream doesn't work, good enough for now
+  console.log("Downloading...");
+  const databaseFile = await sourceS3.send(
+    new GetObjectCommand({ Bucket: sourceBucket, Key: sourceTpDbLocation })
+  );
 
-  await targetS3.send(new PutObjectCommand({
-    Bucket: targetBucket,
-    Key: targetTpDbLocation,
-    Body: data,
-    Metadata:{
-      "disabledencryptionkey": "true"
-    }
-  }));
+  const data = await databaseFile.Body.transformToByteArray(); // readable stream doesn't work, good enough for now
+
+  console.log("Uploading...");
+  await targetS3.send(
+    new PutObjectCommand({
+      Bucket: targetBucket,
+      Key: targetTpDbLocation,
+      Body: data,
+      Metadata: {
+        disabledencryptionkey: "true",
+      },
+    })
+  );
 
   console.log("Done tp_db");
-}
+};
 
-const movePart = async (partName)=>{
-  const sourceMetaFilePath = getMetaPartLocation(sourceAccount,sourceSnapshotName, sourceSnapshotId, partName);
-  const targetMetaFilePath = getMetaPartLocation(targetAccount,targetSnapshotName, targetSnapshotId, partName);
+const movePart = async (partName) => {
+  const sourceMetaFilePath = getMetaPartLocation(
+    sourceAccount,
+    sourceSnapshotName,
+    sourceSnapshotId,
+    partName
+  );
+  const targetMetaFilePath = getMetaPartLocation(
+    targets3ForcePathStyle ? `${targetBucket}/${targetAccount}` : targetAccount,
+    targetSnapshotName,
+    targetSnapshotId,
+    partName
+  );
 
-  console.log(`Uploading part file ${partName} from ${sourceMetaFilePath}, ${sourceBucket} to ${targetMetaFilePath}, ${targetBucket}`);
+  console.log(
+    `Uploading part file ${partName} from ${sourceMetaFilePath}, ${sourceBucket} to ${targetMetaFilePath}, ${targetBucket}`
+  );
 
-  const metaFile = await sourceS3.send(new GetObjectCommand({Bucket: sourceBucket, Key: sourceMetaFilePath}));
+  const metaFile = await sourceS3.send(
+    new GetObjectCommand({ Bucket: sourceBucket, Key: sourceMetaFilePath })
+  );
 
-  const data = await metaFile.Body.transformToByteArray()
+  const data = await metaFile.Body.transformToByteArray();
 
-  await targetS3.send(new PutObjectCommand({
-    Bucket: targetBucket,
-    Key: targetMetaFilePath,
-    Body: data
-  }));
+  await targetS3.send(
+    new PutObjectCommand({
+      Bucket: targetBucket,
+      Key: targetMetaFilePath,
+      Body: data,
+    })
+  );
 
   console.log(`Done ${partName}`);
-}
+};
 
-async function main(){
+async function main() {
   console.log("Starting file transfer process...");
 
   await moveTpDb();
-  await movePart('layout_management.bak');
+  await movePart("layout_management.bak");
 
   console.log("Completed");
 }
 
-
- main();
+main();
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
 - Grab tokens from https://apptio-sso.awsapps.com/start/#/?tab=accounts -> Access keys
 
 
-Update config.env:
+Create config.env in root directory:
 ```
 SOURCE_ACCESS_KEY_ID=
 SOURCE_ACCESS_KEY=
@@ -26,6 +26,16 @@ TARGET_ACCESS_SESSION_TOKEN=
 1. Restore the target snapshot
 
 
-# Notes
 
-Due to permissions we can't copy from prod to staging, so we download/upload instead
+# Handy commands
+
+aws --endpoint-url http://localhost:8093 s3 ls s3://apptio-tp-snapshots-stg/tplocal.com/test_b612f2c2-ae78-4652-a2ab-57c72099269d/
+aws --endpoint-url http://localhost:8093 s3 ls s3://apptio-tp-snapshots-stg --recursive --human-readable --summarize
+
+
+Actions
+-  npm run start
+-  npm run start:local -> to localstack. For some reason migration fails during restore of a snapshot in local-setup, so for tp-db still recommended manual approach
+
+
+
